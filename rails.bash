9d94a8679c76768a37e0d998fb9a50e397e02c56
Jonhnny Weslley <jw@jonhnnyweslley.net>
2011-03-14 23:25:53 -0300

    remove short options, except -e and -d which completes more options

--- rails.bash
+++ rails.bash
@@ -19,7 +19,7 @@
 #
 #  http://github.com/jweslley/rails_completion
 #
-#  VERSION: 0.1.5
+#  VERSION: 0.1.6
 
 
 RAILSCOMP_FILE=".rails_generators~"
@@ -136,7 +136,7 @@ _rails_generate(){
 
   if [ -z "$generator" ]; then
     case "$cur" in
-      -*) __railscomp "-h --help" ;;
+      -*) __railscomp "--help" ;;
       *) __rails_generators ;;
     esac
     return
@@ -154,7 +154,7 @@ _rails_destroy(){
 
   if [ -z "$generator" ]; then
     case "$cur" in
-      -*) __railscomp "-h --help" ;;
+      -*) __railscomp "--help" ;;
       *) __rails_generators ;;
     esac
     return
@@ -186,10 +186,10 @@ _rails_new(){
   esac
 
   case "$prev" in
-    -r*|--ruby=*|-b*|--builder=*|-m*|--template=*) _filedir ;;
-    *) __railscomp "-G --skip-git --dev --edge --skip-gemfile -O --skip-active-record
-      -J --skip-prototype -T --skip-test-unit -s --skip -f --force -p --pretend
-      -q --quiet -h --help -v --version -b -builder= -m --template= -d --database= -r --ruby="
+    --ruby=*|--builder=*|--template=*) _filedir ;;
+    *) __railscomp "--skip-git --dev --edge --skip-gemfile --skip-active-record
+      --skip-prototype --skip-test-unit --skip --force --pretend --quiet --help
+      --version --builder= --template= -d --database= --ruby="
   esac
 }
 
@@ -205,13 +205,13 @@ _rails_server(){
   esac
 
   case "$prev" in
-    -c*|--config=*|-P*|--pid=*) _filedir ;;
-    *) __railscomp "-h --help -P --pid= -e --environment= -u --debugger -d --daemon -c --config= -b --binding= -p --port=" ;;
+    --config=*|--pid=*) _filedir ;;
+    *) __railscomp "--help --pid= -e --environment= --debugger --daemon --config= --binding= --port=" ;;
   esac
 }
 
 _rails_console(){
-  __railscomp "test development production -s --sandbox --debugger"
+  __railscomp "test development production --sandbox --debugger"
 }
 
 _rails_profiler(){
@@ -219,7 +219,7 @@ _rails_profiler(){
   _get_comp_words_by_ref cur
 
   case "$cur" in
-    -*) __railscomp "-h --help" ;;
+    -*) __railscomp "--help" ;;
     *) __railscomp "flat graph graph_html"
   esac
 }
@@ -229,8 +229,8 @@ _rails_plugin(){
   _get_comp_words_by_ref cur prev
 
   case "$prev" in
-    -r*|--root=*) _filedir ;;
-    *) __railscomp "-h --help -v --verbose -r --root= -s --source= install remove" ;;
+    --root=*) _filedir ;;
+    *) __railscomp "--help --verbose --root= --source= install remove" ;;
   esac
 }
 
@@ -246,14 +246,14 @@ _rails_runner(){
   esac
 
   case "$prev" in
-    runner) __railscomp "-h --help -e --environment=" ;;
+    runner) __railscomp "--help -e --environment=" ;;
     -e*|--environment=*) _filedir ;;
     *) COMPREPLY==() ;;
   esac
 }
 
 _rails_benchmarker(){
-  __railscomp "-h --help"
+  __railscomp "--help"
 }
 
 # end of Rails commands --------------------------------------------------------
@@ -263,7 +263,7 @@ _rails(){
   local cur options command commands
   _get_comp_words_by_ref cur
 
-  options="-h --help -v --version"
+  options="--help --version"
   if [[ -f "script/rails" ]]; then
     commands="s server c console g generate destroy profiler plugin runner benchmarker db dbconsole"
   else
